ab8820d188602fdbcc44e974dceab68f622ce9d8
nobozor <m.monet@orange.fr>
2020-06-17 09:08:55 +0200

    modifié

--- pointeur.c
+++ pointeur.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
+void triplepointeur(int *pointeursurnombre)
+{
+	*pointeursurnombre *= 2; // Multiplie la valeur du nombre par 2 
+}
+
 int main() { 
-int A=8;
-int B=2;
-int* ptr;
-ptr=&A;
-printf("%p \n,ptr");
+int A=8; // création de la variable A 
+int B=2; // création de la variable B
+int* ptr=NULL; // création du pointeur 
+ptr=&A; // met l'addresse de A dans mon pointeur
+
+printf("A vaut : %d\n", *ptr); // On utilise *ptr avec avec %d pour afficher la valeur de A
+printf("L'adresse de la variable A est : %p \n",ptr);  // %p pour pointeur
+
+triplepointeur(ptr); // appelle de la fonction triplepointeur
+
+printf("après multiplication : %d \n",*ptr );
+return 0;
 }
